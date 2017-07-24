10c076bf344b6caac584237c07bc6ce1f5e42d4c
Anil Jha <aniljha_91@yahoo.co.in>
2017-07-24 17:57:18 +0530

    creating api gateway repository

---
+++ src/main/java/com/org/gateway/filter/PreRoutingFilter.java
@@ -0,0 +1,59 @@
+package com.org.gateway.filter;
+
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.org.gateway.security.JwtTokenUtil;
+import com.org.gateway.util.ErrorHandler;
+
+@Component
+public class PreRoutingFilter extends ZuulFilter {
+
+	private static Logger log = LoggerFactory.getLogger(PreRoutingFilter.class);
+	
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	
+	@Value("${jwt.header}")
+	private String headerName;
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run(){
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		if (ctx!=null && request != null && request.getHeader(headerName)!=null) {
+			log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+			ctx.addZuulRequestHeader(headerName, request.getHeader(headerName));
+			/*if(jwtTokenUtil.validateToken(request.getHeader(AuthConstants.REQUEST_TOKEN_HEADER_NAME))){
+				ctx.addZuulRequestHeader(AuthConstants.REQUEST_TOKEN_HEADER_NAME, request.getHeader(AuthConstants.REQUEST_TOKEN_HEADER_NAME));
+			}*/
+		}else{
+			new ErrorHandler().processValidationError(new AuthenticationException("invalid user, Please enter valid credentials"));
+		}
+		return null;
+	}
+
+}
---
+++ src/main/java/com/org/gateway/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.org.gateway.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.org.gateway.dao.UserDAO;
+import com.org.gateway.entity.User;
+import com.org.gateway.service.UserService;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+    UserDAO userDAO;
+
+	@Override
+	public User findByName(String userName) {
+		return userDAO.findByName(userName);
+	}
+	
+	@Override
+	public List<User> listOfUsers() {
+		return userDAO.listOfUsers();
+	}
+
+
+}
